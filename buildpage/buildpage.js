093743e7d72b2ea727ee6f0f3a6cf0179f126c62
tomiconicgit <tomiconicx@icloud.com>
2025-08-31 00:06:39 +0100

    Update buildpage.js

--- buildpage/buildpage.js
+++ buildpage/buildpage.js
@@ -5,11 +5,15 @@ import { createCanvas } from './assets/canvasbuilder.js';
  */
 export function initBuildPage() {
     const mainContainer = document.getElementById('build-environment');
+    if (!mainContainer) {
+        console.error("Build environment not found.");
+        return;
+    }
     mainContainer.style.width = '100%';
     mainContainer.style.height = '100%';
     mainContainer.style.display = 'flex';
     mainContainer.style.flexDirection = 'column';
-    mainContainer.style.backgroundColor = 'transparent'; // No additional background, handled by app-wrapper
+    mainContainer.style.backgroundColor = 'transparent'; // No additional background
 
     // Now, create the canvas inside the main container
     createCanvas(mainContainer);
