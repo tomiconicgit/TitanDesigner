ca74b546d393c2fb2e386679ab4f525df953d319
tomiconicgit <tomiconicx@icloud.com>
2025-08-30 22:59:16 +0100

    Update layoutschema.js

--- engine/layoutschema.js
+++ engine/layoutschema.js
@@ -3,15 +3,30 @@
 
 export let layoutSchema =;
 
+/**
+ * Adds a new component object to the central layout schema.
+ * @param {object} component The component to add.
+ */
 export function addComponent(component) {
-    // Logic to add a component to the schema will go here.
+    layoutSchema.push(component);
 }
 
+/**
+ * Updates the properties of an existing component in the schema.
+ * @param {string} id The ID of the component to update.
+ * @param {object} newProps The new properties to assign.
+ */
 export function updateComponent(id, newProps) {
-    // Logic to update a component's properties will go here.
+    const component = layoutSchema.find(c => c.id === id);
+    if (component) {
+        Object.assign(component.props, newProps);
+    }
 }
 
+/**
+ * Generates a unique ID for a new component.
+ * @returns {string} A unique component ID.
+ */
 export function generateId() {
-    // A function to generate unique IDs for components.
     return `comp_${Date.now()}`;
 }
