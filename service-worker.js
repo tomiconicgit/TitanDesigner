9494b8f4405d0da21b38f0f7a1eddf4394c6c763
tomiconicgit <tomiconicx@icloud.com>
2025-08-30 03:28:42 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 // This is a basic service worker. 
 // For now, it just allows the app to be installable (fulfills PWA criteria).
-// We can add caching strategies later.
 
 self.addEventListener('install', (event) => {
   console.log('Service Worker: Installed');
