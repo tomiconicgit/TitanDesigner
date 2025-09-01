1995549e3fa1a6ab6703a718ca037737822c29c8
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 04:00:33 +0100

    Create icon.js

---
+++ viewport/components/icon.js
@@ -0,0 +1,9 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Icon',
+        props: {
+            x: 136, y: 400,
+            width: 48, height: 48
+        }
+    };
+}
