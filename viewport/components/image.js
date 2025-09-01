23d42ef0b5097f5cf1c67108f30dde8c26445715
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 03:59:57 +0100

    Create image.js

---
+++ viewport/components/image.js
@@ -0,0 +1,10 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Image',
+        props: {
+            src: 'https://via.placeholder.com/250x150',
+            x: 25, y: 250,
+            width: 250, height: 150
+        }
+    };
+}
