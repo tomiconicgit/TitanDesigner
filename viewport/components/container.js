1b23a5d42e1a4668724b8c2669a12abf3a160456
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 03:58:56 +0100

    Create container.js

---
+++ viewport/components/container.js
@@ -0,0 +1,9 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Container',
+        props: {
+            x: 50, y: 150,
+            width: 200, height: 150
+        }
+    };
+}
