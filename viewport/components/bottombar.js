f0e290239f98708b4165208974fe1fd42c973104
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 03:59:14 +0100

    Create bottombar.js

---
+++ viewport/components/bottombar.js
@@ -0,0 +1,9 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Bottom Bar',
+        props: {
+            x: 0, y: 750, // Positioned at the bottom
+            width: 320, height: 50
+        }
+    };
+}
