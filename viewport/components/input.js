131a608e9db9b4448bd6adc7fbf3ae93d464a681
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 03:59:39 +0100

    Create input.js

---
+++ viewport/components/input.js
@@ -0,0 +1,10 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Input',
+        props: {
+            placeholder: 'Enter text...',
+            x: 25, y: 200,
+            width: 250, height: 44
+        }
+    };
+}
