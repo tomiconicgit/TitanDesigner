ff676901fd52f2ea22ea5d9b9cc567f72d614e7a
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 03:58:22 +0100

    Create card.js

---
+++ viewport/components/card.js
@@ -0,0 +1,9 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Card',
+        props: {
+            x: 50, y: 150,
+            width: 250, height: 120
+        }
+    };
+}
