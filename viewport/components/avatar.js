96e4e4f2c17054ecc473590e1718622f6d20e59e
tomiconicgit <tomiconicx@icloud.com>
2025-09-01 04:00:18 +0100

    Create avatar.js

---
+++ viewport/components/avatar.js
@@ -0,0 +1,10 @@
+export function createComponentTemplate() {
+    return {
+        type: 'Avatar',
+        props: {
+            src: 'https://via.placeholder.com/64',
+            x: 128, y: 300,
+            width: 64, height: 64
+        }
+    };
+}
